ecb8e311e635f0e637fc0906b8304a89b88f1cd7
Peter Adjao <pa376499@gmail.com>
2026-08-08 05:10:23 +0000

    Learn primitive and object data types

---
+++ index.js
@@ -0,0 +1,23 @@
+let carCount = 0;
+carCount += 1;
+console.log(carCount);
+
+
+const firstName = "Alex";
+const message = `Hello, ${firstName}!`;
+
+console.log(message);
+
+  const age = 25;
+
+  console.log(typeof age);
+
+const user = {
+  name: "Alex",
+  age: 25,
+  email: "example@gmail.com",
+};
+
+console.log(user.email);
+
+console.log(typeof "Alex");
